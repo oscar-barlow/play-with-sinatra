c1f804aeec53a480ffbebdbab9588fc4720fef1f
ajbater <amanda22@hotmail.co.uk>
2016-11-22 10:51:55 +0000

    first commit - learning sinatra

---
+++ app.rb
@@ -0,0 +1,21 @@
+require 'sinatra'
+
+get '/' do
+  "Hello"
+end
+
+get '/secret' do
+  "Hi"
+end
+
+get '/amanda' do
+  "amanda"
+end
+
+get '/oscar' do
+  "oscar"
+end
+
+get '/sinatra' do
+  "learning sinatra"
+end
